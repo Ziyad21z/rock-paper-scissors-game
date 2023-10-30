8a81fbe501e0e27123fc1910afd1f8e065eb64f5
Ziyad <115634322+Ziyad21z@users.noreply.github.com>
2023-10-30 09:18:16 +0300

    finished the game
    i am gonna return to this when i get better at programming to make it better and more efficient.

--- script.js
+++ script.js
@@ -1,45 +1,78 @@
-let playerSelection;
-const computerSelection = getComputerChoice();
-let text = document.getElementById('text').innerText;
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        document.getElementById('text').innerText ='tie'
-    }
-    if (condition) {
-        
-    }
-
+let playerScore = 0;
+let computerScore = 0;
+try {
+    game();    
+} catch (error) {
+    alert('will if you changed your mind reload tha page')
 }
 
-function playerChoice(choice) {
-    // the choise is the the player pick and the id at the same time
-    // when a button is clicked we sent it to the playRound function
-    document.getElementById(choice).onclick = function () {
-        playRound(choice, computerSelection);
+
+function game() {
+    while (true) {
+        const computerSelection = getComputerChoice();
+        const playerSelection = playerChoice();
+        playRound(playerSelection, computerSelection);
+       if(playerScore === 5 || computerScore === 5){
+        break
+       }
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        alert('it\'s a tie try again')
+        console.log(`you ${playerScore} computer ${computerScore}`) 
+    }
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerScore = playerScore + 1;
+        alert('you win') 
+        console.log(`you ${playerScore} computer ${computerScore}`) 
+    }
+    if (playerSelection === 'paper' && computerSelection === 'rock') {
+        playerScore = playerScore + 1;
+        alert('you win')
+        console.log(`you ${playerScore} computer ${computerScore}`) 
+    }
+    if (playerSelection === 'scissors' && computerSelection === 'paper') {
+       playerScore = playerScore + 1;++ playerScore;
+        alert('you win')
+        console.log(`you ${playerScore} computer ${computerScore}`) 
+    }
+    if (computerSelection === 'rock' && playerSelection === 'scissors') {
+        computerScore = computerScore + 1;
+        alert('you lose')
+        console.log(`you ${playerScore} computer ${computerScore}`) 
+    }
+    if (computerSelection === 'paper' && playerSelection === 'rock') {
+        computerScore = computerScore + 1;
+        alert('you lose')
+        console.log(`you ${playerScore} computer ${computerScore}`) 
+    }
+    if (computerSelection === 'scissors' && playerSelection === 'paper') {
+        computerScore = computerScore + 1;
+        alert('you lose')
+        console.log(`you ${playerScore} computer ${computerScore}`) 
+    }
+}
+
+function playerChoice() {
+    // ask the player to enter there choise
+    let playerChoice = window.prompt("type rock or paper or scissors").toLocaleLowerCase()
+    // to make sure he typed correctly
+    while (playerChoice != undefined) {
+        if (playerChoice != 'rock' && playerChoice != 'paper' && playerChoice != 'scissors') {
+            console.log('you must chose rock or paper or scissors!')
+            playerChoice = window.prompt("type rock or paper or scissors")
+        }
+        else
+            // when he type we return there choise
+            return playerChoice
     }
 }
 
 function getComputerChoice() {
-
     // this array have the elemnts the computer can randomly pick from
     let computerChoices = ["rock", "paper", "scissors"]
-    // the g is just 
-    let g = computerChoices[(Math.floor(Math.random() * computerChoices.length))];
-    console.log(g)
     // here we return the chosen elemnt with math functions
-    return g
+    return computerChoices[(Math.floor(Math.random() * computerChoices.length))]
 }
-
-/*
-function rock() {
-    playRound("rock", computerSelection);
-}
-
-function paper() {
-    playerSelection = 'paper'
-}
-
-function scissors() {
-    playerSelection = "scissors"
-}
-*/
